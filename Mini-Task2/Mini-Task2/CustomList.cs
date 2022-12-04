4e85464e725871ddb88784d8be3ff1f89c0c3aaf
FUNman3310 <tu7ldh0l3@code.edu.az>
2022-12-05 01:08:09 +0400

    complited_but_not_all

---
+++ Mini-Task2/Mini-Task2/CustomList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Task2
+{
+    public class CustomList<T>
+    {
+
+        public T[] values = new T[0];
+
+        public void Add(T value)
+        {
+            Array.Resize(ref values, values.Length+1);
+            values[^1] = value;
+        }
+
+        public void Clean()
+        {
+            T[] newValues = new T[0];
+            values = newValues;
+        }
+
+        public int FindIndexOf(T value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (value==values[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static bool  Equals (this T value1,T value2)
+        {
+            if (value1 == value2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+ 
+    }
+}
